d8ad37ff9e946cb6e54d84b4310ebd0b6fca0005
JakeHorsfield <jhorsfield8@gmail.com>
2013-08-13 17:01:54 +0100

    Created an abstract Bullet class which all types of bullets will extend.

---
+++ src/com/des/hidrosity/bullets/Bullet.java
@@ -0,0 +1,66 @@
+package com.des.hidrosity.bullets;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.des.hidrosity.constants.GameConstants;
+import com.jakehorsfield.libld.GameObject;
+
+public abstract class Bullet extends GameObject {
+
+	private enum BulletDirection {
+		Left, Right
+	}
+
+	private BulletDirection direction;
+
+	private Body physicsBody;
+	private World gameWorld;
+	
+	private boolean shouldBeRemoved = false;
+
+	public Bullet(Vector2 position, String textureName, BulletDirection direction, World gameWorld) {
+		super(position, textureName);
+
+		this.direction = direction;
+		this.gameWorld = gameWorld;
+
+		createPhysicsBody();
+	}
+
+	private void createPhysicsBody() {
+		BodyDef bodyDef = new BodyDef();
+		bodyDef.position.set((getX() + (getWidth() * GameConstants.IMAGE_SCALE) / 2) * GameConstants.UNIT_SCALE,
+				(getY() + (getHeight() * GameConstants.IMAGE_SCALE) / 2) * GameConstants.UNIT_SCALE);
+		bodyDef.type = BodyType.DynamicBody;
+
+		PolygonShape polygonShape = new PolygonShape();
+		polygonShape.setAsBox(((getWidth() * GameConstants.IMAGE_SCALE) / 2) * GameConstants.UNIT_SCALE,
+				((getHeight() * GameConstants.IMAGE_SCALE) / 2) * GameConstants.UNIT_SCALE);
+		
+		FixtureDef fixtureDef = new FixtureDef();
+		fixtureDef.friction = 0f;
+		fixtureDef.restitution = 0f;
+		fixtureDef.shape = polygonShape;
+		fixtureDef.density = 1f;
+		
+		physicsBody = gameWorld.createBody(bodyDef);
+		
+		Fixture fixture = physicsBody.createFixture(fixtureDef);
+		fixture.setUserData(this);
+	}
+
+	public void update(float delta) {
+
+	}
+
+	public void render(SpriteBatch spriteBatch) {
+
+	}
+}
