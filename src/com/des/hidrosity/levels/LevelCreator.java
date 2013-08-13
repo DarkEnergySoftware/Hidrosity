867f6ea456110fbafed25de5a7075d3a7579d29e
JakeHorsfield <jhorsfield8@gmail.com>
2013-08-13 12:17:03 +0100

    Increased friction. It still needs some tweaking though

--- src/com/des/hidrosity/levels/LevelCreator.java
+++ src/com/des/hidrosity/levels/LevelCreator.java
@@ -99,7 +99,7 @@ public class LevelCreator {
 
 		FixtureDef fixtureDef = new FixtureDef();
 		fixtureDef.shape = polygonShape;
-		fixtureDef.friction = 100f;
+		fixtureDef.friction = 0.1f;
 
 		Body body = gameWorld.createBody(bodyDef);
 		Fixture fixture = body.createFixture(fixtureDef);
