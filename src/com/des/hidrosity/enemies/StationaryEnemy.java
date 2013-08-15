3c08fe008214d0ee2b0a954e9fec98236a980ff7
JakeHorsfield <jhorsfield8@gmail.com>
2013-08-15 16:38:02 +0100

    The StationaryEnemy now shoots!

--- src/com/des/hidrosity/enemies/StationaryEnemy.java
+++ src/com/des/hidrosity/enemies/StationaryEnemy.java
@@ -1,15 +1,21 @@
 package com.des.hidrosity.enemies;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.TimeUtils;
+import com.des.hidrosity.bullets.StationaryEnemyBullet;
 import com.des.hidrosity.constants.EnemyConstants;
 import com.des.hidrosity.player.Player;
+import com.des.hidrosity.screens.PlayScreen;
 import com.jakehorsfield.libld.Utils;
 
 public class StationaryEnemy extends Enemy {
 
 	private long lastTimeShot;
+	
+	private Array<StationaryEnemyBullet> bullets = new Array<StationaryEnemyBullet>();
 	
 	public StationaryEnemy(Vector2 position, String textureName, Player player) {
 		super(position, textureName, player);
@@ -28,7 +34,23 @@ public class StationaryEnemy extends Enemy {
 	}
 	
 	private void shoot() {
+		if (currentDirection == Direction.Left) {
+			shootBulletFromLeft();
+		} else {
+			shootBulletFromRight();
+		}
 		
+		lastTimeShot = TimeUtils.millis();
+	}
+	
+	private void shootBulletFromRight() {
+		StationaryEnemyBullet b = new StationaryEnemyBullet(new Vector2(getX(), getY()), "res/bullets/jettenBullet.png", 1, PlayScreen.physicsWorld);
+		bullets.add(b);
+	}
+	
+	private void shootBulletFromLeft() {
+		StationaryEnemyBullet b = new StationaryEnemyBullet(new Vector2(getX(), getY()), "res/bullets/jettenBullet.png", -1, PlayScreen.physicsWorld);
+		bullets.add(b);
 	}
 	
 	private boolean shouldShoot() {
@@ -37,6 +59,19 @@ public class StationaryEnemy extends Enemy {
 		}
 		
 		return false;
+	}
+	
+	@Override
+	public void render(SpriteBatch spriteBatch) {
+		super.render(spriteBatch);
+		
+		renderBullets(spriteBatch);
+	}
+	
+	private void renderBullets(SpriteBatch spriteBatch) {
+		for (StationaryEnemyBullet b : bullets) {
+			b.render(spriteBatch);
+		}
 	}
 
 }
