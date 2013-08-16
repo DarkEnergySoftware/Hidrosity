cf721ff376c5bad4275e568b9ccb8dfc375cdfe0
JakeHorsfield <jhorsfield8@gmail.com>
2013-08-16 11:12:28 +0100

    The enemies bullets no longer collide with the enemy. I finally understand collision filtering!

--- src/com/des/hidrosity/bullets/StationaryEnemyBullet.java
+++ src/com/des/hidrosity/bullets/StationaryEnemyBullet.java
@@ -1,11 +1,17 @@
 package com.des.hidrosity.bullets;
 
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Filter;
 import com.badlogic.gdx.physics.box2d.World;
+import com.des.hidrosity.constants.CollisionConstants;
 
 public class StationaryEnemyBullet extends Bullet {
 
 	public StationaryEnemyBullet(Vector2 position, String textureName, int direction, World gameWorld) {
 		super(position, textureName, direction, gameWorld);
+		
+		Filter filterData = new Filter();
+		filterData.categoryBits = CollisionConstants.ENEMY;
+		filterData.maskBits = CollisionConstants.ENEMY_MASK;
 	}
 }
