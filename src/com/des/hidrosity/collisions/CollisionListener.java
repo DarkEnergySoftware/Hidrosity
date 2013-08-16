04aca0a330f2f8f4e4e11ff5defa369cc242e2cf
JakeHorsfield <jhorsfield8@gmail.com>
2013-08-16 12:16:20 +0100

    Reverted CollisionListener

--- src/com/des/hidrosity/collisions/CollisionListener.java
+++ src/com/des/hidrosity/collisions/CollisionListener.java
@@ -4,57 +4,42 @@ import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.ContactImpulse;
 import com.badlogic.gdx.physics.box2d.ContactListener;
 import com.badlogic.gdx.physics.box2d.Manifold;
-import com.des.hidrosity.bullets.Bullet;
-import com.des.hidrosity.bullets.PlayerBullet;
 import com.des.hidrosity.player.Player;
-import com.des.hidrosity.screens.PlayScreen;
 
 public class CollisionListener implements ContactListener {
 
 	private int numPlayerCollisions;
 
-	private Object fixtureAUserData;
-	private Object fixtureBUserData;
-
 	public void beginContact(Contact contact) {
-		fixtureAUserData = contact.getFixtureA().getUserData();
-		fixtureBUserData = contact.getFixtureB().getUserData();
-
-		if (userDataIsNull(contact)) {
+		if (contact.getFixtureA().getUserData() == null || contact.getFixtureB().getUserData() == null) {
 			return;
 		}
 
 		checkIfPlayerTouchesGround(contact);
 	}
 
-	private boolean userDataIsNull(Contact contact) {
-		if (fixtureAUserData == null || fixtureBUserData == null) {
-			return true;
-		}
-
-		return false;
-	}
-
 	private void checkIfPlayerTouchesGround(Contact contact) {
-		if (fixtureAUserData instanceof Player && fixtureBUserData.toString().equals("level")) {
+		if (contact.getFixtureA().getUserData() instanceof Player
+				&& contact.getFixtureB().getUserData().toString().equals("level")) {
 			numPlayerCollisions++;
 
 			if (numPlayerCollisions > 0) {
-				((Player) fixtureBUserData).setCanJump(true);
+				((Player) contact.getFixtureB().getUserData()).setCanJump(true);
 			}
 		}
 
-		if (fixtureBUserData instanceof Player && fixtureAUserData.toString().equals("level")) {
+		if (contact.getFixtureB().getUserData() instanceof Player
+				&& contact.getFixtureA().getUserData().toString().equals("level")) {
 			numPlayerCollisions++;
 
 			if (numPlayerCollisions > 0) {
-				((Player) fixtureBUserData).setCanJump(true);
+				((Player) contact.getFixtureB().getUserData()).setCanJump(true);
 			}
 		}
 	}
 
 	public void endContact(Contact contact) {
-		if (userDataIsNull(contact)) {
+		if (contact.getFixtureA().getUserData() == null || contact.getFixtureB().getUserData() == null) {
 			return;
 		}
 
@@ -62,34 +47,31 @@ public class CollisionListener implements ContactListener {
 	}
 
 	private void checkIfPlayerLeavesGround(Contact contact) {
-		if (fixtureAUserData instanceof Player && fixtureBUserData.toString().equals("level")) {
+		if (contact.getFixtureA().getUserData() instanceof Player
+				&& contact.getFixtureB().getUserData().toString().equals("level")) {
 			numPlayerCollisions--;
 
-			if (playerCanJump()) {
-				((Player) fixtureAUserData).setCanJump(false);
+			if (numPlayerCollisions <= 0) {
+				((Player) contact.getFixtureA().getUserData()).setCanJump(false);
 			}
 		}
 
-		if (fixtureBUserData instanceof Player && fixtureAUserData.toString().equals("level")) {
+		if (contact.getFixtureB().getUserData() instanceof Player
+				&& contact.getFixtureA().getUserData().toString().equals("level")) {
 			numPlayerCollisions--;
 
-			if (playerCanJump()) {
-				((Player) fixtureBUserData).setCanJump(false);
+			if (numPlayerCollisions <= 0) {
+				((Player) contact.getFixtureB().getUserData()).setCanJump(false);
 			}
 		}
-	}
-
-	private boolean playerCanJump() {
-		if (numPlayerCollisions <= 0) {
-			return true;
-		}
-
-		return false;
 	}
 
 	public void preSolve(Contact contact, Manifold oldManifold) {
+
 	}
 
 	public void postSolve(Contact contact, ContactImpulse impulse) {
+
 	}
+
 }
