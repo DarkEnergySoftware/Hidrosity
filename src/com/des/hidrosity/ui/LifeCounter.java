5cf5a022bb4d4eb9681bbc88f514c712d9d5525f
Jake Horsfield <jhorsfield8@gmail.com>
2013-08-25 16:25:18 +0100

    Added a shadow to the life counter text

--- src/com/des/hidrosity/ui/LifeCounter.java
+++ src/com/des/hidrosity/ui/LifeCounter.java
@@ -1,6 +1,7 @@
 package com.des.hidrosity.ui;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -28,6 +29,9 @@ public class LifeCounter {
 	
 	public void render(SpriteBatch spriteBatch) {
 		spriteBatch.draw(texture, 10, Gdx.graphics.getHeight() - 138 - texture.getHeight()*2, texture.getWidth()*2, texture.getHeight()*2);
+		font.setColor(Color.GRAY);
+		font.draw(spriteBatch, String.valueOf(player.getLives()), 80, Gdx.graphics.getHeight() - 160);
+		font.setColor(Color.WHITE);
 		font.draw(spriteBatch, String.valueOf(player.getLives()), 78, Gdx.graphics.getHeight() - 158);
 	}
 	
