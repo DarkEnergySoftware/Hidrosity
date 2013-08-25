6569229f6cb70721582c8b3cb5e24764cbed3d97
Jake Horsfield <jhorsfield8@gmail.com>
2013-08-25 15:44:47 +0100

    Added text to the life counter

--- src/com/des/hidrosity/ui/LifeCounter.java
+++ src/com/des/hidrosity/ui/LifeCounter.java
@@ -2,7 +2,9 @@ package com.des.hidrosity.ui;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.des.hidrosity.characters.CharacterManager;
 import com.des.hidrosity.player.Player;
 
@@ -12,14 +14,21 @@ public class LifeCounter {
 	
 	private Texture texture;
 	
+	private FreeTypeFontGenerator fontGenerator;
+	private BitmapFont font;
+	
 	public LifeCounter(Player player) {
 		this.player = player;
 		
 		texture = CharacterManager.getCharacter().lifeCounterTexture;
+		
+		fontGenerator = new FreeTypeFontGenerator(Gdx.files.internal("res/ui/lifesFont.ttf"));
+		font = fontGenerator.generateFont(14);
 	}
 	
 	public void render(SpriteBatch spriteBatch) {
 		spriteBatch.draw(texture, 10, Gdx.graphics.getHeight() - 138 - texture.getHeight()*2, texture.getWidth()*2, texture.getHeight()*2);
+		font.draw(spriteBatch, String.valueOf(player.getLives()), 78, Gdx.graphics.getHeight() - 158);
 	}
 	
 }
