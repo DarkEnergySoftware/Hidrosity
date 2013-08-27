46698e87e92343bee120c92b213d8afda59a72e0
Jake Horsfield <jhpy1024@gmail.com>
2013-08-27 17:33:01 +0100

    Added fade in to the game over screen

--- src/com/des/hidrosity/screens/GameOverScreen.java
+++ src/com/des/hidrosity/screens/GameOverScreen.java
@@ -1,5 +1,8 @@
 package com.des.hidrosity.screens;
 
+import aurelienribon.tweenengine.Tween;
+import aurelienribon.tweenengine.TweenManager;
+
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
@@ -7,7 +10,9 @@ import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.des.hidrosity.tweens.SpriteTweenAccessor;
 import com.jakehorsfield.libld.Utils;
 
 public class GameOverScreen implements Screen {
@@ -15,41 +20,74 @@ public class GameOverScreen implements Screen {
 	private SpriteBatch spriteBatch;
 	private Texture texture;
 	
+	private Sprite sprite;
+	
+	private TweenManager tweenManager;
+
 	public void show() {
 		spriteBatch = new SpriteBatch();
 		texture = Utils.loadTexture("res/menus/game over/texture.png");
+
+		sprite = new Sprite(texture);
+		sprite.setBounds(0, 0, texture.getWidth(), texture.getHeight());
 		
+		tweenManager = new TweenManager();
+		
+		createFadeInTween();
+
 		Gdx.input.setInputProcessor(new Input());
 	}
-	
+
+	private void createFadeInTween() {
+		Tween.registerAccessor(Sprite.class, new SpriteTweenAccessor());
+		Tween.set(sprite, SpriteTweenAccessor.ALPHA).target(0f)
+				.start(tweenManager);
+		Tween.to(sprite, SpriteTweenAccessor.ALPHA, 0.8f).target(1)
+				.start(tweenManager);
+	}
+
 	public void render(float delta) {
+		updateTween();
+		
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
+
 		spriteBatch.begin();
 		{
-			spriteBatch.draw(texture, 0, 0);
+			sprite.draw(spriteBatch);
 		}
 		spriteBatch.end();
+	}
+
+	private void updateTween() {
+		tweenManager.update(Gdx.graphics.getDeltaTime());
 	}
 
 	public void hide() {
 		Gdx.input.setInputProcessor(null);
 	}
-	
-	public void resize(int width, int height) {}
-	public void pause() {}
-	public void resume() {}
-	public void dispose() {}
-	
+
+	public void resize(int width, int height) {
+	}
+
+	public void pause() {
+	}
+
+	public void resume() {
+	}
+
+	public void dispose() {
+	}
+
 	class Input implements InputProcessor {
 
 		public boolean keyDown(int keycode) {
 			switch (keycode) {
 			case Keys.ENTER:
-				((Game) Gdx.app.getApplicationListener()).setScreen(new GameScreen());
+				((Game) Gdx.app.getApplicationListener())
+						.setScreen(new GameScreen());
 			}
-			
+
 			return false;
 		}
 
@@ -61,7 +99,8 @@ public class GameOverScreen implements Screen {
 			return false;
 		}
 
-		public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		public boolean touchDown(int screenX, int screenY, int pointer,
+				int button) {
 			return false;
 		}
 
@@ -80,6 +119,6 @@ public class GameOverScreen implements Screen {
 		public boolean scrolled(int amount) {
 			return false;
 		}
-		
+
 	}
 }
