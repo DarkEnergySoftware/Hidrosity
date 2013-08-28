ba9df7dee2c3fd6465639fdf0f5b04d4e3715bf6
Jake Horsfield <jhpy1024@gmail.com>
2013-08-28 21:16:12 +0100

    Changed GameOver screen

--- src/com/des/hidrosity/constants/PlayerConstants.java
+++ src/com/des/hidrosity/constants/PlayerConstants.java
@@ -9,11 +9,13 @@ public class PlayerConstants {
 	public static final float SPEED = 0.02f;
 	public static final float JUMP_FORCE = 1f;
 	public static final float MAX_VELOCITY = 5f;
+	public static final float KNOCKBACK_FORCE = 0.5f;
+	public static final float DYING_TIME = 1f;
+	
 	public static final long SHOOT_DELAY = 500;
 	public static final long SPAWN_TIME = 500;
 	public static final long SHOOTING_TIME = 250;
 	public static final long HURT_TIME = 400;
+	
 	public static final int HEALTH_DECREASE = 1;
-	public static final float KNOCKBACK_FORCE = 0.5f;
-	public static final float DYING_TIME = 1f;
 }
